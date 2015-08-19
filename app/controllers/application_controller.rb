70ee26237f1450457b7b89182bbe36f6080e3eb2
mcburnham <mattburnham2014@gmail.com>
2015-08-19 17:31:25 +0000

    pls 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,18 @@ class ApplicationController < Sinatra::Base
     set :views, "app/views"
 
   end
-  
+
   get "/" do
+    @all_yaks = Yak.all
+    puts @all_yaks
+    erb :index
+    
+  end
   
+  post '/new_yak' do
+    Yak.create({:message => params[:message]})
+    redirect to "/"
+    
+  end
+  
+end
