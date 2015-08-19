5595b9d9bcff75081a802f0770d2d5c286be7696
mcburnham <mattburnham2014@gmail.com>
2015-08-19 17:28:54 +0000

    pls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ require "./config/environment"
 require "./app/models/Yak"
 
 class ApplicationController < Sinatra::Base
-  
+
   configure do
     set :public_folder, "public"
     set :views, "app/views"
-    
+
   end
   
   get "/" do
-    @
+  
