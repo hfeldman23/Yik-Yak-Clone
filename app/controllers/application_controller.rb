a19140bf9201ced8b9cbd4e2ae6c4ad0d0774b62
Henry Feldman <hfeldman23@gmail.com>
2015-08-24 17:53:21 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, "public"
     set :views, "app/views"
-
   end
 
   get "/" do
     @all_yaks = Yak.all
     puts @all_yaks
     erb :index
-
   end
 
   post '/new_yak' do
@@ -22,7 +20,7 @@ class ApplicationController < Sinatra::Base
     redirect "/"
   end
 
-  post'/login' do
+  post '/login' do
     User.create({:username => params[:username], :email => params[:email]})
     redirect "/"
   end
